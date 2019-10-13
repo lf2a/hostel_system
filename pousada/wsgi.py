1ed824d292dc9fccba43f105be641476d1f8dc06
Luiz Filipy <luizfilipy014@gmail.com>
2019-10-13 15:24:45 -0300

    deploy

--- pousada/wsgi.py
+++ pousada/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for pousada project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
+# python standard library
 import os
 
+# django
 from django.core.wsgi import get_wsgi_application
+
+# external library
+from dj_static import Cling
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pousada.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
