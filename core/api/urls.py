ea0b3901342a12c80ed6485e8f327c5ed908dfeb
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-29 16:03:17 -0300

    change endpoint basnename

--- core/api/urls.py
+++ core/api/urls.py
@@ -7,7 +7,7 @@ from core.api.views import SignUpViewSet, LogoutViewSet
 router = routers.DefaultRouter()
 
 # /api/signup/
-router.register('signup', SignUpViewSet, basename='signup')
+router.register('signup', SignUpViewSet, basename='signup-api')
 
 # /api/logout/
-router.register('logout', LogoutViewSet, basename='logout')
+router.register('logout', LogoutViewSet, basename='logout-api')
